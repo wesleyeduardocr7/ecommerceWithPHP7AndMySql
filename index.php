dfe1204c618cd7add3a5a59a55c793278d13a02f
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-03-09 19:40:34 -0300

    criandoPagePadrao

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <?php 
 
 require_once("vendor/autoload.php");
+use \Slim\Slim;
+use Classes\Page;
 
-$app = new \Slim\Slim();
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK";
+	$page = new Page();
+
+	$page->setTpl("index"); 
 
 });
 
