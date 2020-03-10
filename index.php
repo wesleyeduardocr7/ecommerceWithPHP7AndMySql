6aaef481c810ddb83dfb2e75cd57abe8e7cd109a
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-03-10 09:09:13 -0300

    criandoPageAdmin

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use Classes\Page;
+use Classes\PageAdmin;
 
 $app = new Slim();
 
@@ -13,6 +14,14 @@ $app->get('/', function() {
 	$page = new Page();
 
 	$page->setTpl("index"); 
+
+});
+
+$app->get('/admin', function() {
+    
+	$pageAdmin = new PageAdmin();
+
+	$pageAdmin->setTpl("index"); 
 
 });
 
