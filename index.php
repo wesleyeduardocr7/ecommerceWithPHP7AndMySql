bd401ac1be24206d4ab5d52cebc3a08eec71f1c8
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-03-22 16:31:44 -0300

    CRUD

--- index.php
+++ index.php
@@ -90,6 +90,16 @@ $app->get('/admin/users/create',function(){
 $app->get("/admin/users/:iduser/delete",function($iduser){
 
 	User::verifyLogin();
+	
+	$user = new User();
+	
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+
+	exit;
 
 });
 
@@ -98,9 +108,15 @@ $app->get('/admin/users/:iduser',function($iduser){
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get((int)$iduser);
+
 	$page = new PageAdmin();
 
-	$page->setTpl("users-update");
+	$page->setTpl("users-update",array(
+		"user"=>$user->getValues()
+	));
 
 });
 
@@ -117,15 +133,29 @@ $app->post("/admin/users/create",function(){
 
 	$user->save();
 
-	var_dump($user);
+	header("Location: /admin/users");
 
-	header("Location: /admin/user");
+	exit;
 
 });
 
 $app->post("/admin/users/:iduser",function($iduser){
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+
+	exit;
 
 });
 
