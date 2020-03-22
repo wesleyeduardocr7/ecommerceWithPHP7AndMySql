c507355d7c84ea783d60b17d58791b04fed86200
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-03-22 11:38:22 -0300

    createUsers

--- vendor/classes/php-classes/src/Model/User.php
+++ vendor/classes/php-classes/src/Model/User.php
@@ -68,6 +68,31 @@ class User extends Model{
     }
 
     
+    public static function listAll(){
+
+        $sql = new Sql();
+
+       return  $result = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING (idperson) ORDER BY b.desperson");
+
+    }
+
+    public function save()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>utf8_decode($this->getdesperson()),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+
+	}
 
 
 }
--- views-cache/teste.34720f6c883cf1454e3b28cc80b60f01.rtpl.php
+++ views-cache/teste.34720f6c883cf1454e3b28cc80b60f01.rtpl.php
@@ -1,9 +1,9 @@
-<!-- Content Wrapper. Contains page content -->
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
         <h1>
-            Page Headers
+            Page Header WESLEY EDUARDO OK USERS 5
             <small>Optional description</small>
         </h1>
         <ol class="breadcrumb">
