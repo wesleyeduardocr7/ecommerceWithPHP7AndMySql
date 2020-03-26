3941b05c16324748bc2db09932ca7a0fd0c74591
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-03-26 10:37:12 -0300

    ProdutosPorPagina

--- vendor/classes/php-classes/src/Model/Category.php
+++ vendor/classes/php-classes/src/Model/Category.php
@@ -108,7 +108,7 @@ class Category extends Model {
 
 	}
 
-	public function getProductsPage($page = 1, $itemsPerPage = 8)
+	public function getProductsPage($page = 1, $itemsPerPage = 3)
 	{
 
 		$start = ($page - 1) * $itemsPerPage;
@@ -135,6 +135,7 @@ class Category extends Model {
 		];
 
 	}
+
 
 	public function addProduct(Product $product)
 	{
